9474ffdb5df301f8c4668cc7640a8cd69405d88f
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2014-07-26 19:53:50 -0500

    Auctex autocomplete

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-mode-hook (quote (LaTeX-preview-setup LaTeX-math-mode turn-on-reftex)))
+ '(LaTeX-mode-hook (quote (LaTeX-preview-setup LaTeX-math-mode turn-on-reftex)) t)
  '(TeX-PDF-mode t)
  '(TeX-auto-save t)
  '(TeX-engine (quote xetex))
@@ -15,6 +15,7 @@
  '(canlock-password "373e129cafb77ade03ef1a7c57a3321b8966c25b")
  '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+ '(flyspell-use-meta-tab nil)
  '(focus-follows-mouse t)
  '(font-lock-maximum-decoration t)
  '(fuel-factor-root-dir "/home/krzys/progs/factor/")
--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,12 +3,15 @@
 (require-package 'fuel)
 (require-package 'auctex)
 (require-package 'reftex)
+(require-package 'auto-complete-auctex)
+(require 'auto-complete-auctex)
 
 (setq slime-path (file-name-directory (locate-library "slime")))
 (setq slime-backend (expand-file-name "swank-loader.lisp" slime-path))
 (setq slime-lisp-implementations
      '((sbcl ("sbcl" "--core" "/home/krzys/prog-local/sbcl.core-for-slime")) (clisp ("/usr/bin/clisp"))))
 
+(ac-flyspell-workaround)
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 
 (provide 'init-local)
