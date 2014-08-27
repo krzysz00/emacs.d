5a43dc9bdc8591798075c6687fd9b3faa80e3e2a
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2014-08-27 09:14:37 -0500

    Remove some heavy packages for UTCS

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,15 +1,12 @@
-(require-package 'j-mode)
 (require-package 'android-mode)
 (require-package 'fuel)
 (require-package 'auctex)
 (require-package 'reftex)
-(require-package 'auto-complete-auctex)
-(require 'auto-complete-auctex)
 
 (setq slime-path (file-name-directory (locate-library "slime")))
 (setq slime-backend (expand-file-name "swank-loader.lisp" slime-path))
 (setq slime-lisp-implementations
-     '((sbcl ("sbcl" "--core" "/home/krzys/prog-local/sbcl.core-for-slime")) (clisp ("/usr/bin/clisp"))))
+     '((sbcl ("sbcl")) (clisp ("/usr/bin/clisp"))))
 
 (ac-flyspell-workaround)
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
