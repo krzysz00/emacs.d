468cf91cc0cf4b8cf75895969d52535c3a4cb15d
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2016-05-15 11:00:23 -0500

    Rework things for company-mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,14 +5,21 @@
 
 (require-package 'auctex)
 (require-package 'reftex)
+(require-package 'company-auctex)
 
 (setq slime-path (file-name-directory (locate-library "slime")))
 (setq slime-backend (expand-file-name "swank-loader.lisp" slime-path))
 (setq slime-lisp-implementations
-     '((sbcl ("sbcl" "--core" "/home/krzys/prog-local/sbcl.core-for-slime")) (clisp ("/usr/bin/clisp"))))
+      '((sbcl ("sbcl" "--core" "/home/krzys/prog-local/sbcl.core-for-slime")) (clisp ("/usr/bin/clisp"))))
 
-(ac-flyspell-workaround)
-(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+(after-load 'company
+  (define-key company-mode-map (kbd "M-TAB") 'company-complete)
+  (define-key company-active-map (kbd "M-TAB") 'company-select-next)
+  (add-hook 'TeX-mode-hook
+            (lambda ()
+              (make-local-variable 'company-backends)
+              (company-auctex-init))))
+
 (add-hook 'text-mode-hook 'flyspell-mode)
 
 (load "l.el") ;; TODO: Temp mode for Dr. Dillig's class
