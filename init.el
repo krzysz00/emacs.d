55615c7387ed7220f9fb09334744885e26b1ab68
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2018-10-05 16:00:55 -0700

    Scala init

--- init.el
+++ init.el
@@ -108,6 +108,7 @@
 (require 'init-yaml)
 (require 'init-docker)
 (require 'init-terraform)
+(require 'init-scala)
 ;;(require 'init-nix)
 (maybe-require-package 'nginx-mode)
 
---
+++ lisp/init-scala.el
@@ -0,0 +1,7 @@
+(require-package 'scala-mode)
+(require-package 'sbt-mode)
+(require-package 'ensime)
+
+(setq ensime-startup-notification nil)
+
+(provide 'init-scala)
