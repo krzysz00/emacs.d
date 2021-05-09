2ea97c61d30850250042e23be16f5be6fc3452cc
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2021-05-09 10:41:34 -0500

    Shrink font because HiDPI, customize saved a few settings

--- custom.el
+++ custom.el
@@ -22,6 +22,7 @@
  '(auth-sources (quote ("~/.authinfo.gpg" "~/.authinfo" "~/.netrc")))
  '(c-basic-offset 4)
  '(canlock-password "373e129cafb77ade03ef1a7c57a3321b8966c25b")
+ '(column-number-mode t)
  '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
    (quote
@@ -47,20 +48,22 @@
  '(org-log-done nil)
  '(package-selected-packages
    (quote
-    (company-auctex reftex auctex company-coq proof-general racket-mode fuel android-mode envrc uptimes shfmt dotenv-mode daemons htmlize lua-mode gnuplot sudo-edit flycheck-ledger ledger-mode origami regex-tool info-colors flycheck-clojure cider elein cljsbuild-mode clojure-mode slime-company slime cask-mode flycheck-relint cl-libify flycheck-package highlight-quoted macrostep cl-lib-highlight aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav paredit nginx-mode ensime sbt-mode scala-mode company-terraform terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode flycheck-rust racer rust-mode flycheck-nim nim-mode j-mode flycheck-ocaml merlin-eldoc merlin tuareg sqlformat projectile-rails yard-mode bundler yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax psci psc-ide purescript-mode flycheck-elm elm-test-runner elm-mode dhall-mode dante haskell-mode reformatter toml-mode company-anaconda anaconda-mode pip-requirements restclient httprepl haml-mode css-eldoc skewer-less sass-mode rainbow-mode tagedit org-pomodoro writeroom-mode org-cliplink company-php smarty-mode php-mode add-node-modules-path skewer-mode js-comint coffee-mode prettier-js typescript-mode js2-mode json-mode erlang csv-mode markdown-mode textile-mode crontab-mode alert ccls lsp-mode ibuffer-projectile github-review forge github-clone bug-reference-github yagist git-commit magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed vc-darcs browse-at-remote whitespace-cleanup-mode which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters goto-line-preview beacon mode-line-bell vlf list-unicode-display unfill mmm-mode session windswap switch-window company-quickhelp company marginalia consult-flycheck embark-consult projectile consult embark selectrum-prescient selectrum flycheck-color-mode-line flycheck ibuffer-vc wgrep anzu diff-hl diredfl disable-mouse default-text-scale dimmer solarized-theme command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq dsvn hippie-expand-slime goto-gem cmd-to-echo darcsum expand-region ivy-xref swiper counsel ivy smex paredit-everywhere guide-key sqlup-mode hindent intero magithub ruby-mode git-messenger nlinum ivy-historian cl-lib idris-mode less-css-mode sql-indent scss-mode org-fstree nix-sandbox nix-buffer magit-gh-pulls go-mode github-issues dynamic-spaces company-nixos-options)))
+    (company-auctex reftex auctex company-coq proof-general racket-mode fuel android-mode envrc uptimes shfmt dotenv-mode daemons htmlize lua-mode gnuplot sudo-edit flycheck-ledger ledger-mode origami regex-tool info-colors flycheck-clojure cider elein cljsbuild-mode clojure-mode slime-company slime cask-mode flycheck-relint cl-libify flycheck-package highlight-quoted macrostep cl-lib-highlight aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav paredit nginx-mode ensime sbt-mode scala-mode company-terraform terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode flycheck-rust racer rust-mode flycheck-nim nim-mode j-mode flycheck-ocaml dune-format dune merlin-eldoc merlin-company merlin tuareg sqlformat projectile-rails yard-mode bundler yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax psci psc-ide purescript-mode flycheck-elm elm-test-runner elm-mode dhall-mode dante haskell-mode reformatter toml-mode company-anaconda anaconda-mode pip-requirements restclient httprepl haml-mode css-eldoc skewer-less sass-mode rainbow-mode tagedit org-pomodoro writeroom-mode org-cliplink company-php smarty-mode php-mode add-node-modules-path skewer-mode js-comint coffee-mode prettier-js typescript-mode js2-mode json-mode erlang csv-mode markdown-mode textile-mode crontab-mode alert ccls lsp-mode ibuffer-projectile github-review forge github-clone bug-reference-github yagist git-commit magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed vc-darcs browse-at-remote whitespace-cleanup-mode which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters beacon mode-line-bell vlf list-unicode-display unfill mmm-mode session windswap switch-window company-quickhelp company marginalia consult-flycheck embark-consult projectile consult embark selectrum-prescient selectrum flycheck-color-mode-line flycheck ibuffer-vc wgrep anzu diff-hl diredfl disable-mouse default-text-scale dimmer solarized-theme command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq goto-line-preview dsvn hippie-expand-slime goto-gem cmd-to-echo darcsum expand-region ivy-xref swiper counsel ivy smex paredit-everywhere guide-key sqlup-mode hindent intero magithub ruby-mode git-messenger nlinum ivy-historian cl-lib idris-mode less-css-mode sql-indent scss-mode org-fstree nix-sandbox nix-buffer magit-gh-pulls go-mode github-issues dynamic-spaces company-nixos-options)))
  '(python-shell-interpreter "python3" t)
  '(reftex-plug-into-AUCTeX t)
  '(scss-compile-at-save nil t)
  '(sentence-end-double-space nil)
  '(session-use-package t nil (session))
+ '(show-paren-mode t)
  '(solarized-high-contrast-mode-line t)
  '(solarized-scale-org-headlines nil)
  '(solarized-use-less-bold t)
  '(tab-width 4)
+ '(tool-bar-mode nil)
  '(verilog-auto-lineup (quote ignore)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit t :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 211 :width normal :foundry "PfEd" :family "Inconsolata")))))
+ '(default ((t (:inherit t :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 132 :width normal :foundry "PfEd" :family "Inconsolata")))))
