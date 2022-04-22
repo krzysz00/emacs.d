aaa55626832e6bc4c228eb0fbc235d5e36b8510c
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2022-04-21 20:47:06 -0500

    Use victor voice

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
 (let ((file (expand-file-name "emacspeak/lisp/emacspeak-setup.el" user-emacs-directory)))
   (when (file-exists-p file)
     (load-file file)
-    (dtk-set-language "en-us")
+    (dtk-set-language "en-us:victor")
     (dtk-set-rate 420 t)
     (emacspeak-toggle-word-echo t)))
 
